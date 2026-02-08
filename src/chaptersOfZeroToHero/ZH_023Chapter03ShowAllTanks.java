661de8d4c46968db3de1d052b1d8700cf58c2a5f
Marco Servetto <marco.servetto@gmail.com>
2026-02-08 22:35:19 +1300

    Update ZH_023Chapter03ShowAllTanks.java

--- src/chaptersOfZeroToHero/ZH_023Chapter03ShowAllTanks.java
+++ src/chaptersOfZeroToHero/ZH_023Chapter03ShowAllTanks.java
@@ -19,43 +19,64 @@ To do this, we will use mutation:
 
 Since each tanks is represented as 3 lines, we will have a grid 30 * 10.
 Initially, every cell will contain `      `: six spaces, representing the absence of a tank. This is because each tank line is 6 characters.
-```
-TanksToS: F[List[Tank],Str]{
-  #(ts) -> Block#
-    .let res = {0.range(30).map{_->this.newLine}.list }
-    .do{ ts.flow.forEach{ t -> Block#
-      .let x= { t.position.x }
-      .let y= { t.position.y }
-      .if {x.inRange(0,10).not} .done
-      .if {y.inRange(0,10).not} .done
-      .do{ res.get(y * 3)    .get(x).set(t.repr1) }
-      .do{ res.get(y * 3 + 1).get(x).set(t.repr2) }
-      .do{ res.get(y * 3 + 2).get(x).set(t.repr3) }
-      .done
-      }}
-    .return { res.flow.map{::flow.join(``)}.join(`\n`) },
-  .newLine: mut List[mut Var[Str]] -> 0.range(10).map{ _ -> Vars#(`      `).list },
+-------------------------*/@Test void showAllTanks1(){ run("""
+//OMIT_START
+use base.Main as Main;
+use base.Str as Str;
+use base.Var as Var;
+use base.Vars as Vars;
+use base.Void as Void;
+use base.Int as Int;
+use base.Nat as Nat;
+use base.Bool as Bool;
+use base.F as F;
+use base.ToStr as ToStr;
+use base.List as List;
+use base.Block as Block;
+use base.Sealed as Sealed;
+use base.WidenTo as WidenTo;
+use base.Output as Output;
+Tank:{.position:Point }
+Point:{.x:Int;.y:Int}
+NextState:F[List[Tank],List[Tank]]{::}
+//OMIT_END
+TanksToS: F[List[Tank],Str]{ ts -> Block#
+  .let res = {0.rangeUntil(30).map{_->this.newLine}.list }
+  .do{ ts.flow.forEach{ t -> Block#
+    .let x= { t.position.x }
+    .let y= { t.position.y }
+    .if {x.inRange(0,10).not} .done
+    .if {y.inRange(0,10).not} .done
+    .do{ res.get(y * 3)    .get(x).set(t.repr1) }
+    .do{ res.get(y * 3 + 1).get(x).set(t.repr2) }
+    .do{ res.get(y * 3 + 2).get(x).set(t.repr3) }
+    .done
+    }}
+  .return { res.flow.map{::.flow.join(``)}.join(``|) };
+  .newLine: mut List[mut Var[Str]] -> 0.rangeUntil(10).map{ _ -> Vars#(`      `)}.list;
   }
-
 PrintGame: {
-  mut .fs: mut FS,
-  mut .singleLine(ts: List[Tank]): Void -> this.fs.println(TanksToS#(ts))
-  mut .lines(rounds: Num, ts: List[Tank]): Void -> Block#
+  mut .out: mut Output;
+  mut .singleLine(ts: List[Tank]): Void -> this.out.println(TanksToS#(ts));
+  mut .lines(rounds: Nat, ts: List[Tank]): Void -> Block#
     .var current= {ts}
-    .return{ 0.range(rounds).forEach{step -> Block#(
-      this.fs.println(`\nStep `+step+`\n`),
-      this.fs.println(`\n------------------------------------------------------------\n`),
+    .return{ 0.rangeUntil(rounds).forEach{step -> Block#(
+      this.fs.println(`Step `+step|),
+      this.fs.println(`------------------------------------------------------------`|),
       this.singleLine(ts.get),
-      this.fs.println(`\n------------------------------------------------------------\n`),
+      this.fs.println(`------------------------------------------------------------`|),
       ts.set(NextState#(ts.get))
-      )}},
+      )}}
   }
-ReadGame: { ... }
+//ReadGame: { ... }
+ReadGame: { .cap[T]:T; .read:List[Tank]->{};}
 Test: Main {sys -> Block#
-  .let fs= {UnrestrictedIO#sys}
-  .let game= ReadGame{fs}.read
-  .return{ PrintGame{fs}.lines(50,game) }
-```
+  .let out= {sys.out}
+//  .let in= {sys.inputFile}
+  .let game= ReadGame{20}.read
+  .return{ PrintGame{out}.lines(50,game) }
+  }
+"""); }/*--------------------------------------------
 
 As you can see, we omitted the code reading the initial game state.
 This is because in order to read data from files there is still quite some content that we need to learn. We will handle that in Chapter 4.
