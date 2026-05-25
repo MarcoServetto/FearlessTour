7d25f8ae381820de41e475756380ffaef3481f95
Marco Servetto <marco.servetto@gmail.com>
2026-05-25 16:17:21 +1200

    Update CompileHtml.java

--- src/compileHtml/CompileHtml.java
+++ src/compileHtml/CompileHtml.java
@@ -1,13 +1,16 @@
 package compileHtml;
 
 import java.nio.file.Path;
+
+import mainCoordinator.ResolveResource;
+
 import java.io.IOException;
 import markDownTests.DocumentProcessor;
 import markDownTests.HtmlCreator;
 public class CompileHtml {
   public static void main(String[] args) throws IOException{
-    Path root=Path.of("C:\\")
-      .resolve("Users","Lardo","OneDrive","Documents","GitHub","FearlessTour","src","chaptersOfZeroToHero"); 
+    Path root=
+            ResolveResource.commonsSrc.getParent().getParent().resolve("FearlessTour","src","chaptersOfZeroToHero"); 
     var dest=     //Path.of("htmlOut");
       root.resolve("..").resolve("..").resolve("..").resolve("ZeroToHero").resolve("src").resolve("assetsGuide");
     var creator=  new HtmlCreator(dest);
